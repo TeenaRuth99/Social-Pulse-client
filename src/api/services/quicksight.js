2a457f88da1e9e29ee30c43019f2276dd2b37223
TeenaRuth99 <91128178+TeenaRuth99@users.noreply.github.com>
2023-06-27 21:54:18 +0530

    Update quicksight.js

--- src/api/services/quicksight.js
+++ src/api/services/quicksight.js
@@ -1,15 +1,13 @@
-import { stringify } from 'query-string';
-import * as http from './http';
-import ApiService from './service';
+import * as http from "./http";
+import ApiService from "./service";
 
 export class QuickSightService extends ApiService {
-    // async getEmbeddedURL(request) {
-    //     const url =
-    //         //`${this.apiDomain}/api/dashboard_links/quicksight`;
-    //         `http://localhost:3004/${request.resource}`;
-    //     const response = await http
-    //         .get(url, this.store)
-    //         .catch((err) => console.log(err));
-    //     return { data: response.data };
-    // }
+  async getEmbeddedURL(request) {
+    const url = `${this.apiDomain}/api/dashboard_links/quicksight/${request.check}`;
+
+    const response = await http
+      .get(url, this.store, request.check)
+      .catch((err) => console.log(err));
+    return { data: response.data };
+  }
 }
